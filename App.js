6b23d5115302aba829313f260be0780d4d30cf87
Sagar Gupta <guptasg384@gmail.com>
2023-09-18 00:44:12 +0530

    passing props,dynamic data using api

--- App.js
+++ App.js
@@ -1,23 +1,412 @@
-import React, { Component } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
+const Header = () => {
+  return (
+    <div className="navbar">
+      <div className="logo-container">
+        <img src="https://img.freepik.com/free-vector/hand-drawn-healthy-food-logo_23-2149651916.jpg?w=740&t=st=1694962881~exp=1694963481~hmac=c604e99cb3a08ef0953164ac57fd88c8195915b1bbc79428c9401df2432755e3" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>Conatct Us</li>
+          <li>About</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-// react Element
-const heading = React.createElement("h1", { id: "heading" }, "Namaste React ");
+const ResturantCard = (props) => {
+  const { resData } = props;
 
+   // Check if resData exists before accessing its properties
+   if (!resData || !resData.info) {
+    return null; // or display an error message or loading indicator
+  }
 
-// React Component 
-//  - Class Base 
-//  - Functional base 
-//  -React functional Component is just a javascript function which returns some JSX 
+  return (
+    <div className="res-card">
+      <img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/t4kk0xeepxcvxnkwa2cm"
+        alt="Resturant Logo"
+      />
+      <h3>{resData.info.name}</h3>
+      <h4>{resData.info.cuisines.join(', ')}</h4>
+      <h4>{resData.info.areaName}</h4>
+    </div>
+  );
+};
 
-const HeadingComponent =()=>{
-    return (
-        <>
-    <h1> React Functional Component </h1>
-    <h2> MultiLine react component</h2>
-    </>
-    )
-}
+// Data for a resturant
+const resList = [
+  {
+    info: {
+      id: "136605",
+      name: "Domino's Pizza",
+      cloudinaryImageId: "tutllxiyqliwbien8cob",
+      locality: "Mohalla Baruzai",
+      areaName: "Ghanta Ghar",
+      costForTwo: "\u20B9400 for two",
+      cuisines: ["Pizzas", "Italian", "Pastas", "Desserts"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "136605",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 1400,
+          },
+          {
+            name: "BASE_TIME",
+            fee: 1000,
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2400,
+      },
+      parentId: "2456",
+      avgRatingString: "4.1",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 30,
+        serviceability: "SERVICEABLE",
+        slaString: "30 mins",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-09-17 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "\u20B9150 OFF",
+        subHeader: "ABOVE \u20B9999",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/dominos-pizza-mohalla-baruzai-ghanta-ghar-shahjahanpur-136605",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "133826",
+      name: "Apna Swagat Dhaba",
+      cloudinaryImageId: "krsqzlejjffiv8by79r6",
+      locality: "Samiti Gate",
+      areaName: "Ahmadpur Niwazpur",
+      costForTwo: "\u20B9300 for two",
+      cuisines: ["North Indian"],
+      avgRating: 4.1,
+      feeDetails: {
+        restaurantId: "133826",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 1400,
+          },
+          {
+            name: "BASE_TIME",
+            fee: 1000,
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2400,
+      },
+      parentId: "34952",
+      avgRatingString: "4.1",
+      totalRatingsString: "5K+",
+      sla: {
+        deliveryTime: 26,
+        lastMileTravel: 4.1,
+        serviceability: "SERVICEABLE",
+        slaString: "26 mins",
+        lastMileTravelString: "4.1 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-09-17 23:59:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png",
+            description: "OnlyOnSwiggy",
+          },
+          {
+            imageId: "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png",
+            description: "OnlyOnSwiggy",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "OnlyOnSwiggy",
+                  imageId:
+                    "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png",
+                },
+              },
+              {
+                attributes: {
+                  description: "OnlyOnSwiggy",
+                  imageId:
+                    "v1690360529/Ratnesh_Badges/Only_on_swiggy_badge_4x.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "\u20B9125 OFF",
+        subHeader: "ABOVE \u20B9249",
+        discountTag: "FLAT DEAL",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        badgeType: "BADGE_TYPE_OFFER_PACK",
+        offerSubText: "with offer pack",
+        nonSwiggyOneFreedelMessage: "FREE DELIVERY",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/apna-swagat-dhaba-samiti-gate-ahmadpur-niwazpur-shahjahanpur-133826",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "133910",
+      name: "Luxmi Hotel And Shagun Sweets",
+      cloudinaryImageId: "oif2c18oc3tofcohuopb",
+      locality: "Shahjahanpur Station Road",
+      areaName: "Station Road",
+      costForTwo: "\u20B9200 for two",
+      cuisines: ["North Indian"],
+      avgRating: 3.8,
+      veg: true,
+      feeDetails: {
+        restaurantId: "133910",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 1400,
+          },
+          {
+            name: "BASE_TIME",
+            fee: 1000,
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 2400,
+      },
+      parentId: "126936",
+      avgRatingString: "3.8",
+      totalRatingsString: "1K+",
+      sla: {
+        deliveryTime: 26,
+        lastMileTravel: 3.6,
+        serviceability: "SERVICEABLE",
+        slaString: "26 mins",
+        lastMileTravelString: "3.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-09-17 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "30% OFF",
+        subHeader: "UPTO \u20B975",
+      },
+      loyaltyDiscoverPresentationInfo: {
+        badgeType: "BADGE_TYPE_OFFER_PACK",
+        offerSubText: "with offer pack",
+        nonSwiggyOneFreedelMessage: "FREE DELIVERY",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/luxmi-hotel-and-shagun-sweets-station-road-shahjahanpur-133910",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "132503",
+      name: "Fauzi Tourist Family Dhaba",
+      cloudinaryImageId: "dmkbwhksl3b6drcinq5s",
+      areaName: "Shahjahanpur Locality",
+      costForTwo: "\u20B9250 for two",
+      cuisines: ["North Indian"],
+      avgRating: 3.9,
+      feeDetails: {
+        restaurantId: "132503",
+        fees: [
+          {
+            name: "BASE_DISTANCE",
+            fee: 4400,
+          },
+          {
+            name: "BASE_TIME",
+            fee: 1000,
+          },
+          {
+            name: "ANCILLARY_SURGE_FEE",
+          },
+        ],
+        totalFee: 5400,
+      },
+      parentId: "79057",
+      avgRatingString: "3.9",
+      totalRatingsString: "500+",
+      sla: {
+        deliveryTime: 41,
+        lastMileTravel: 10.3,
+        serviceability: "SERVICEABLE",
+        slaString: "41 mins",
+        lastMileTravelString: "10.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2023-09-17 23:58:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "\u20B9125 OFF",
+        subHeader: "ABOVE \u20B9249",
+        discountTag: "FLAT DEAL",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          mediaType: "ADS_MEDIA_ENUM_IMAGE",
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+    },
+    analytics: {},
+    cta: {
+      link: "https://www.swiggy.com/restaurants/fauzi-tourist-family-dhaba-shahjahanpur-locality-shahjahanpur-132503",
+      type: "WEBLINK",
+    },
+  }
+];
+
+const Body = () => {
+
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-continer">
+        <ResturantCard
+          resName="Meghana Food"
+          cuisine="Pizzas, Fast Food"
+          add="Shahar Khas Basti Begam"
+        />
+        <ResturantCard resData={resList[0]} />
+        <ResturantCard resData={resList[1]}/>
+        <ResturantCard resData={resList[2]}/>
+        <ResturantCard resData={resList[3]}/>
+      </div>
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<HeadingComponent/>);
+root.render(<AppLayout />);
--- README.md
+++ README.md
@@ -1,21 +1,21 @@
-// 
+//
 "devDependencies": {
-    "parcel": "^2.9.3" 
-    
-  }
+"parcel": "^2.9.3"
+
+}
 ^caret for minor update in package
-    // ~tilde for major update in package
+// ~tilde for major update in package
 
 #parcel job
 Dev Build
 creates Local server
 HMR- Hot Module Replacement
 File watching algorithm written in C++
-Faster Builds becuse of caching 
+Faster Builds becuse of caching
 image Optimization
 Compressing Files
 Bundling
-consistant Hashing 
+consistant Hashing
 Code Splitting
 Differential Bundling -support all browesers and their versions
 Diagnostic
@@ -24,27 +24,69 @@ also host on HTTPs
 Tree shaking -remove unuse code
 different dev and production build
 
+JSX = HTML like or XML like syntax JS Syntex
 
-JSX = HTML like or XML like syntax 
-in JSX we can write HTML inside javascript , CSS inside javascript 
---JSX (transpiled before it reaches the JS) 
+--JSX (transpiled before it reaches the JS)
 Babel which is a node library in JS transpiled the JSX code into JS
 
 1. React.createElement => ReactElement-JS object => HTMLElement (render)
+   const heading = React.createElement("h1", { id: "heading" }, "Namaste React ");
 2. JSX (Babel Converted / Transpiled)=> React.createElement => ReactElement-JS object => HTMLElement (render)
 
-
-// React Component 
-//  - Class Base 
-//  - Functional base 
-//  -React functional Component is just a javascript function which returns some JSX 
-    -React Component named in camel case 
-    -We render react components by <HeadingComponent/>
+// React Component
+// - Class Base
+// - Functional base
+// -React functional Component is just a javascript function which returns some JSX
+-React Component named in camel case
+-We render react components by <HeadingComponent/>
 const HeadingComponent =()=>{
-    return (
-        <>
-    <h1> React Functional Component </h1>
-    <h2> MultiLine react component</h2>
-    </>
-    )
+return (
+<>
+<h1> React Functional Component </h1>
+<h2> MultiLine react component</h2>
+</>
+)
 }
+
+inside JSX if we want to write JS then inside {} we write any JS code
+
+_Component Composition _
+writing componennts inside another component
+
+calling functional components in react
+
+ <Title />
+      {Title()}
+      <Title> <Title/>
+
+props are known as passing properties of component , or passing arguments to a function
+
+<ResturantCard
+          resName="Meghana Food"
+          cuisine="Pizzas, Fast Food"
+           add="Shahar Khas Basti Begam"
+ />
+here resName , cuisine ,add are props of component <ResturantCard/>
+-React take this all props and wrap inside an JS object and pass it to the actual component
+
+--Destructring props on the fly
+const ResturantCard = ({resName,cuisine,add}) => {
+return (
+<div className="res-card">
+<img
+        className="res-logo"
+        src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/t4kk0xeepxcvxnkwa2cm"
+        alt="Resturant Logo"
+      />
+<h3>{resName} </h3>
+<h4>{cuisine}</h4>
+<h4>{add}</h4>
+now we directly use props
+</div>
+);
+};
+
+
+*ConfigDriven UI*
+controling/creating  the UI using data 
+Ui is Driven by Config (Data/API)
